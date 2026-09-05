7691acf7086b042c2cb1cbe2f7748d7cfc055136
khatriasim <khatriasim111@gmail.com>
2026-09-05 11:19:48 +0545

    fix trusted host for railway

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ app = FastAPI(
 )
 app.add_middleware(
     TrustedHostMiddleware,
-    allowed_hosts=["localhost", "127.0.0.1", "testserver"]
+    allowed_hosts=["localhost", "127.0.0.1", "testserver", "*.railway.app","fastapi-production-5801.up.railway.app", ]
 )
 
 app.include_router(api_router)
